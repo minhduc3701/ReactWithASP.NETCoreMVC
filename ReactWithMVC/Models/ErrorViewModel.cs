d085324550b6494e80c59b36d161b08e325e8feb
Duc Nguyen Minh <nmd@aod.vn>
2022-03-10 01:51:07 +0700

    create

---
+++ ReactWithMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ReactWithMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ReactWithMVC/wwwroot/js/App.jsx
@@ -0,0 +1,9 @@
+﻿export default class App extends React.Component {
+    render() {
+        return (
+            <div className="commentBox">React with asp.net core</div>
+            );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
